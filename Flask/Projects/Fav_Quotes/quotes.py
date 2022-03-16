73bd7233f5eb8cb50c722d1c0ccc454dbf179d77
ahmedmds <example@example>
2022-03-16 02:09:51 +0100

    Add project files for Fav_Quotes

---
+++ Flask/Projects/Fav_Quotes/quotes.py
@@ -0,0 +1,49 @@
+from flask import Flask, render_template, request, redirect, url_for
+from flask_sqlalchemy import SQLAlchemy
+
+app = Flask(__name__)
+
+
+# For database on local machine, using Valentina Studio, create database 'quote' for user 'postgres', replace <pwd> with password
+# app.config['SQLALCHEMY_DATABASE_URI'] = 'postgresql+psycopg2://postgres:<pwd>@localhost/quotes'
+
+# For deployed app on Heroku, once the app is created (but before deployment) on Heroku, the Postgres database is provisioned via 'Resources' on Heroku, replace <pwd> with password
+app.config['SQLALCHEMY_DATABASE_URI'] = 'postgres://jzxrgqdegvowqw:bbee417256be2e0eb15f11101f6c127854b12bebd90dea546fc9185a1aea69b7@ec2-52-19-164-214.eu-west-1.compute.amazonaws.com:5432/d2e499be71bnot'
+
+app.config['SQLALCHEMY_TRACK_MODIFICATIONS']= False
+
+db = SQLAlchemy(app)
+
+class Favquotes(db.Model):
+	id = db.Column(db.Integer, primary_key=True)
+	author = db.Column(db.String(30))
+	quote = db.Column(db.String(2000))
+
+# For both local machine and Heroku-deployed versions:
+# Now to create the table in the already created database 'quotes':
+# Open Terminal (from within VSCode possible) and go to project directory .\Projects\Fav_Quotes
+# Open Python (venv) .\Projects\Fav_Quotes>python
+# >>> from quotes import db
+# >>> db.create_all()
+# >>> exit()
+
+
+
+@app.route('/')
+def index():
+    result = Favquotes.query.all() # Fetches and stores all records from table 'favquotes' to the variable 'result'
+    return render_template('index.html', result=result)
+
+@app.route('/quotes')
+def quotes():
+    return render_template('quotes.html')
+
+@app.route('/process', methods =['POST'])
+def process():
+    author = request.form['author']
+    quote = request.form['quote']
+    quotedata = Favquotes(author=author, quote=quote)
+    db.session.add(quotedata)
+    db.session.commit()
+
+    return redirect(url_for('index')) # 'index' refers to the view function index()
